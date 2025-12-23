5f72f02a1aeb67ad236de1f4729c6624825a3014
monroydiego91 <diego.monroyg@uanl.edu.mx>
2025-12-22 20:08:31 -0600

    ui: se agregó la pantalla home a la aplicacion

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-// import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar } from 'expo-status-bar'; 
-import { StyleSheet, Text, View, Image } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen'; // Vista de Login
+import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return (
-    <LoginScreen/> // mostramos la pantalla de login
-  );
-}
+  <StatusBar hidden = {true}/>
+  // definimos una constante para manejar el estado de login
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // por defecto está en falso
+  // definimos una constante para almacenar el email del usuario
+  const [userEmail, setUserEmail] = useState('');  // Almacenamos el correo del usuario (en este caso vacio) pero como string
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#8711b9', 
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  // funcion para manera el inicio exitoso de sesion, recibe como parametro el email
+  const handleLoginSuccess = (email) => {
+    setUserEmail(email); // utilizamos el email recibido para actualizarlo al valor de userEmail
+    setIsLoggedIn(true); // cuando se recibe el correo se actualiza isLogged a true
+  };
+  // función para manejar el cierre de sesión
+  const handleLogout = () => {
+    setIsLoggedIn(false); // volvemos a pasar a falso el estado del login
+    setUserEmail(''); // limpiamos el email del usuario
+  };
+
+  // Lógica de decisión
+  if (isLoggedIn) { // si isLoggedIn es true
+          return <HomeScreen userEmail={userEmail} onLogout={handleLogout} />; 
+  }
+
+  return <LoginScreen onLoginSuccess={handleLoginSuccess} />; 
+}
